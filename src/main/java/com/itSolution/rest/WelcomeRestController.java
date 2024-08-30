5679f525f919d69216bed52d767f7ce03dc26937
suman Kumar <sandeep50493raj@gmail.com>
2024-08-30 14:17:52 +0530

    initial commit

---
+++ src/main/java/com/itSolution/rest/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package com.itSolution.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/")
+	public String getMsg() {
+		return "Welcome to OAuthentication App...!!!";
+	}
+
+}
